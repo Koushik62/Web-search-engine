0a5dd2bbac1c5cc490f09cffdfe5d0f640bfafb7
Koushik <gurramkoushik8@gmail.com>
2023-05-31 20:57:35 +0530

    updated

--- main.py
+++ main.py
@@ -7,32 +7,58 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from bs4 import BeautifulSoup
 
-s = Service('chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+# Define the chrome driver service
+s = Service('chromedriver.exe') 
 
-url = " https://leetcode.com/problemset/all/?page="
+#Initiate the webdriver
+driver = webdriver.Chrome(service=s)  
+
+# the base url of the page to scrap
+page_url ="https://leetcode.com/problemset/all/?page=" 
+
+
+#function to get all 'a' tags from a given url
 
 def get_a_tags(url):
+    
+    #load the url in the browser
     driver.get(url)
-    time.sleep(1)
-    html= driver.page_source
-    soup =BeautifulSoup(html,'html.parser')
-    all_ques = soup.find_all('a')
-    pattern = "/problem/"
-    ans = []
-    for i in all_ques:
-      try:
-         if pattern in i.get('href'):
-             ans.append(i.get('href'))
-      except:
-          pass
+    #wait for 5 seconds to ensure that the page is fully loaded
+    time.sleep(5)
+    #find all the 'a' elements on the page
+    links= driver.find_elements(By.TAG_NAME,"a")
+    ans =[]
+    #iterate over each element
+    for i in links:
+        try: 
+            #check if '/problems/' is the href of 'a' element
+            if "/problems/" in i.get_attribute("href"):
+            #if it is append it to the list of links
+             ans.append(i.get_attribute("href"))
+        except:
+            pass   
+        #remove the duplicate elements using set
+    ans = list(set(ans))
     return ans
-  
-for i in range(1,56):
-      my_ans=get_a_tags(url+str(i))
-      time.sleep(5)
-      with open('lc.txt','a') as f:
-          for j in my_ans:
+
+#list to store the final list of links
+my_ans =[]
+# loop through all pages(1,55)
+
+for i in range(1,55):
+    my_ans+=(get_a_tags(page_url+str(i))) # in each and every page add all questions
+    
+    #removing the duplicates
+my_ans=list(set(my_ans))
+    
+    
+#open a file to write the results
+
+with open('leetcode.txt','a') as f:
+    #iterate over each link in your final list
+    
+   for j in my_ans:
               f.write(j+'\n')
-            
-driver.quit()
+print(len(my_ans))
+
+driver.quit()   
